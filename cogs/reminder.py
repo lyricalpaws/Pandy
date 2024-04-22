29b48e003f889a66baf5aeaa0477092d8cc610c8
Chloe Carver-Brown <26838744+lyricalpaws@users.noreply.github.com>
2024-04-22 13:51:06 +0100

    adding medication reminder timer
    Signed-off-by: Chloe Carver-Brown <26838744+lyricalpaws@users.noreply.github.com>

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,4 +1,5 @@
 import datetime
+import discord
 from discord.ext import commands, tasks
 from utils import default
 from zoneinfo import ZoneInfo
@@ -8,16 +9,26 @@ class reminder(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = default.get("config.json")
-        self.my_task.start()
+        self.medsReminder.start()
 
     def cog_unload(self):
-        self.my_task.cancel()
+        self.medsReminder.cancel()
 
     time = datetime.time(hour=11, minute=00, tzinfo=ZoneInfo('Europe/London'))
 
     @tasks.loop(time=time)
-    async def my_task(self):
-        print("My task is running!")
+    async def medsReminder(self,ctx):
+        for cutie in self.config.reminders:
+            try:
+                user = self.bot.get_user(cutie)
+                embed = discord.Embed(title="Remember to take your medication!!!!",
+                      description="If you haven't taken it yet then now is the best time to do it ❤️",
+                      colour=0xe11399,
+                      timestamp=datetime.datetime.now())
+                embed.set_footer(text="Love you dork")
+                await user.send(embed=embed)
+            except Exception as e:
+                print(e)
 
 
 async def setup(bot):
